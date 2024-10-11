9c415527d333bbd230862410678ed9f557cefded
Awais Shahid <i212764@nu.edu.pk>
2024-10-12 02:53:44 +0500

    WIP:: Autofund with 1 ETH on startup

--- ignition/modules/DIDRegistry.ts
+++ ignition/modules/DIDRegistry.ts
@@ -1,8 +1,10 @@
 // SPDX-License-Identifier: MIT
 
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
+import fundAccount from "../../source/fundAccount";
 export default buildModule("DIDRegistry", (m) => {
     const DIDReg = m.contract("DIDRegistry");
+
+    const task = m.call("fundAccount", fundAccount, DIDReg.address, "1.0");
     return { DIDReg };
 });
