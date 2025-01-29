8b7e0677b4b45986c059d5e308956c6c65206b23
Awais Shahid <i212764@nu.edu.pk>
2025-01-29 09:01:25 +0500

    hardcoded

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,16 @@
-import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from "hardhat/config";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.27",
+  defaultNetwork: "hardhat",
+  networks: {
+    localhost: {
+      accounts: [
+        "0x2a871d0798f97d79848a013d4936a73bf4cc922c825d33c1cf7073dff6d409c6",
+      ],
+    },
+  },
 };
 
 export default config;
