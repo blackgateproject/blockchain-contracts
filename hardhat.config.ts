0d9307d00bcbca273638e03361dc5964586b7784
Awais Shahid <i212764@nu.edu.pk>
2025-01-31 03:15:22 +0500

    CHANGE:: Port to 8546 from 8545

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,8 +7,10 @@ const config: HardhatUserConfig = {
   networks: {
     localhost: {
       accounts: [
-        "0x2a871d0798f97d79848a013d4936a73bf4cc922c825d33c1cf7073dff6d409c6",
+        "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
       ],
+      url: "http://localhost:8546",
+      
     },
   },
 };
